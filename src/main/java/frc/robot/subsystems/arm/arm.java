78daf67df4106c3b37281bfc517844806af3f5fb
gil-liGrota <gilgrota@gmail.com>
2025-09-04 23:43:12 +0300

    finish arm

--- src/main/java/frc/robot/subsystems/arm/arm.java
+++ src/main/java/frc/robot/subsystems/arm/arm.java
@@ -1,5 +1,26 @@
 package frc.robot.subsystems.arm;
 
-public class arm {
+import org.littletonrobotics.junction.Logger;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class arm extends SubsystemBase{
+    private armIO armIO;
+    public armIOInputsAutoLogged armInputs = new armIOInputsAutoLogged();
+
+    public arm(armIO io){
+        this.armIO = io;
+
+        
+    }
+
+    @Override
+    public void periodic() {
+        armIO.updateInputs(armInputs);
+        Logger.processInputs("Arm/arm", armInputs);
+    }
+
+    public armIO getIO(){
+        return armIO;
+    }
 }
