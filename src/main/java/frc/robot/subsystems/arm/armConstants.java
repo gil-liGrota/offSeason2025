a04ba55dc06aa27d633d6fb1998fbda30efb8907
gil-liGrota <gilgrota@gmail.com>
2025-09-09 15:02:36 +0300

    id

--- src/main/java/frc/robot/subsystems/arm/armConstants.java
+++ src/main/java/frc/robot/subsystems/arm/armConstants.java
@@ -1,7 +1,7 @@
 package frc.robot.subsystems.arm;
 
 public class armConstants {
-    public static final int ARM_ID = 16;// TODO check id
+    public static final int ARM_ID = 17;// TODO check id
     public static final int FOLD_SWITCH_ID = 0;// TODO check id
     public static final double KP = 0.0;
     public static final double KI = 0.0;
