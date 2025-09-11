8d3835912c61609684848fbb5c4b2c75bfcaa77d
gil-liGrota <gilgrota@gmail.com>
2025-09-11 22:59:53 +0300

    /

--- src/main/java/frc/robot/subsystems/Elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/Elevator/ElevatorConstants.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems.Elevator;
 public class ElevatorConstants {
     public static final int ELEVATOR_ID = 16;
     public static final int FOLD_SWITCH = 1;
-    public static final int BRAKE_SWITCH = 2;
+    // public static final int BRAKE_SWITCH = 2;
 
     public static final double KP = 1.4;
     public static final double KI = 0;
