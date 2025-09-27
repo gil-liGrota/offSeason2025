50144c6ef62d1a36b24a6c6c80e0517bbcc4e51d
gil-liGrota <gilgrota@gmail.com>
2025-09-27 14:24:43 +0300

    //

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -37,12 +37,14 @@ public final class Constants {
   public static class VisionConstants {
     // THE translations FOR THE LEFT AND RIGHT CAMERAS
     public static Translation3d l_camera_translation = new Translation3d(0.045, -0.23 + 0.24, 0.263);
-    public static Translation3d r_camera_translation = new Translation3d(0.02, 0.307 - 0.4, 0.263);
+    public static Translation3d r_camera_translation = new Translation3d(0.02 - 0.02 + 0.09 + 0.01,
+        0.307 + 0.3 - 0.6 - 0.01 - 0.02,
+        0.263);
 
     // THE ROTATION FOR THE LEFT AND RIGHT CAMERAS
     public static Rotation3d l_camera_rotation = new Rotation3d(Degrees.of(0), Degrees.of(0), Degrees.of(7.8 + 8));
     public static Rotation3d r_camera_rotation = new Rotation3d(Degrees.of(0), Degrees.of(0),
-        Degrees.of(-22.6 + 8 + 5));
+        Degrees.of(-22.6 + 8 + 5 + 1 - 7 + 10 - 3));
 
     // THE TRANSFORMATION FOR THE LEFT AND RIGHT CAMERAS
     public static Transform3d l_camera_transform = new Transform3d(l_camera_translation, l_camera_rotation); // here
